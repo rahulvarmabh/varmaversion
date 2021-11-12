14768a4da9464e1352030551a644c4e46f8fadec
rahulvarmabh <rahulvarmabh@gmail.com>
2021-11-12 17:35:19 +0530

    Salary View and Validations

--- project1/Models/Salary.cs
+++ project1/Models/Salary.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace project1.Models
 {
     public class Salary
     {
+        [Required(ErrorMessage = "Enter Employee ID")]
         public int empid { get; set; }
+        [Required(ErrorMessage = "Enter Name")]
         public string empname { get; set; }
+        [Required(ErrorMessage = "Enter your BasicPay")]
         public double basicpay { get; set; }
         public double da { get; set; }
         public double hra { get; set; }
